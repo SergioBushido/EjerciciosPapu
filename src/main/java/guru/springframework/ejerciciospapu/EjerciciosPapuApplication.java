f6fd97aae5756cda1ff52faa1fd3a8876f1524bb
Sergio <sergio.ume@hotmail.com>
2024-06-04 11:56:12 +0100

    inicio

---
+++ src/main/java/guru/springframework/ejerciciospapu/EjerciciosPapuApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.ejerciciospapu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosPapuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosPapuApplication.class, args);
+	}
+
+}
