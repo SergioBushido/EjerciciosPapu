00ad3408aa6fb1be2ae1328fcbb838e50537b05d
Sergio <sergio.ume@hotmail.com>
2024-06-24 18:57:40 +0100

    Ejercicio7 Resuelto

--- src/main/java/guru/springframework/ejerciciospapu/EjerciciosPapuApplication.java
+++ src/main/java/guru/springframework/ejerciciospapu/EjerciciosPapuApplication.java
@@ -3,11 +3,35 @@ package guru.springframework.ejerciciospapu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Comparator;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
 @SpringBootApplication
 public class EjerciciosPapuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjerciciosPapuApplication.class, args);
-	}
 
+		// Crear un conjunto ordenado en sentido decreciente
+		Set<Integer> numeros = new TreeSet<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		});
+
+		// Crear un generador de números aleatorios
+		Random r = new Random();
+
+		// Generar números aleatorios y añadirlos al conjunto
+		while (numeros.size() < 20) {
+			int numero = r.nextInt(100);
+			numeros.add(numero);
+		}
+
+		// Mostrar la colección
+		System.out.println("Colección en orden decreciente: " + numeros);
+	}
 }
